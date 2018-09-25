d3cdd4483fa217eae1debf15681cd50c658a671d
Mac 10 <zecosc16@htlkaindorf.at>
2018-09-25 09:38:23 +0200

    created all Classes

--- Ex_102/src/ex_102/FileComparer.java
+++ Ex_102/src/ex_102/FileComparer.java
@@ -9,13 +9,6 @@ package ex_102;
  *
  * @author oskar
  */
-public class Ex_102 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class FileComparer {
     
 }
